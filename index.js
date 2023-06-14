ad8fe16814dba6a0c36a632f541ecf5017e11b43
Abogeerick <abogeerick@gmail.com>
2023-06-14 15:50:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
+  const button = document.getElementById('button');
+  
+  button.addEventListener('click', function() {
+    // Code to be executed when the button is clicked
+  });
 }
+
+// Call the addingEventListener() function
+addingEventListener();
